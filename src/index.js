9a59fc0d37c76205b830e4735ea59208713d5f6b
Vinayak Sareen <databunker@Vinayaks-MacBook-Pro.local>
2019-10-23 15:00:52 +0100

    .gitignore file added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Registration from '../src/Registration';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const applicationLayout = ()=>{
+	return (
+		<div>
+			<Registration/>
+		</div>
+	)
+}
+
+
+ReactDOM.render(<applicationLayout />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
