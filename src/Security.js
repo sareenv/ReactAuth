830e3a20ec96e968d60d48e6d7d2bc79a35bc0ee
vinayak <sareenv@uni.coventry.ac.uk>
2019-12-08 21:31:51 +0000

    changes

--- src/Security.js
+++ src/Security.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { List } from 'antd';
+import 'antd/dist/antd.css';
 import 'antd/dist/antd.css';
 
-// this thing doesn't need to be connected but it's there
+// this thing doesn't need to be connected but it's 
+
+
 class Security extends React.Component {
 
-    render() {
-        return(
+    constructor() {
+        super()
+        this.state = {
+            isTwoFactorOn: false
+        }
+    }
+
+    customElement() {
+        return (
             <div>
-                <h3> Security Settings</h3>
+                <h3> Returning something new </h3>
+            </div>
+        )
+    }
+    
+
+    render() {
+
+        const data1 = [{title: 'Logout from All devices', description: 'This option allows users to logout the account from all devices'}, 
+            {title: 'Enable two factor Authentication', description: 'This option will send email verification password to registered email before each login'}, 
+            {title: 'Change current password', description: 'This will allow users to change the current account password'}
+        ]
+        return(
+            <div style={{backgroundColor: "white"}}>
+                <h3 style={{ marginBottom: 16, marginLeft: 16, marginTop: 16, paddingTop: 10}}>Security Settings</h3>
+                <List
+                    itemLayout="horizontal"
+                    size="large"
+                    bordered
+                    dataSource = {data1}
+                    renderItem={item => (
+                        <List.Item>
+                            {item.title}
+                            <List.Item.Meta
+                                description={item.description} 
+                            />
+
+                        </List.Item>   
+                    )}
+                />
             </div>
         )
     }
